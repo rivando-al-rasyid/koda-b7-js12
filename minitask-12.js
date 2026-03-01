0a38f026139d842a647302d5f012224271cbf40c
Rivando Al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2026-03-01 13:40:11 +0700

    Add files via upload

--- minitask-12.js
+++ minitask-12.js
@@ -13,28 +13,52 @@ function customLowerCase(text){
 return hasil
 }
 
-fetch(url).then(response =>{
-  if (!response.ok){
+// fetch(url).then(response =>{
+//   if (!response.ok){
+//     throw response.status
+//   }
+//     return response.json()
+// }).then(
+//   result => {
+//     let cusLowerCaseEmail=[]
+//     let toLowerCaseEmail=[]
+//     result.forEach(user => {
+//       let email = user.email.toLowerCase();
+//       toLowerCaseEmail.push(email);
+//     });
+//       result.forEach(user => {
+//       let cusEmail = customLowerCase(user.email);
+//       cusLowerCaseEmail.push(cusEmail)
+//     });
+//     console.log(toLowerCaseEmail);
+//     console.log(cusLowerCaseEmail);
+
+
+// }).catch(err => {
+//   console.log(err)
+// });
+
+(async () => {
+  try {
+    const response = await fetch(url)
+      if (!response.ok){
     throw response.status
   }
-    return response.json()
-}).then(
-  result => {
-    let cusLowerCaseEmail=[]
-    let toLowerCaseEmail=[]
+  const result = await response.json();
+
+    let email = result.map(user => user.email.toLowerCase());
+    let cusLowerCaseEmail = [];
     result.forEach(user => {
-      let email = user.email.toLowerCase();
-      toLowerCaseEmail.push(email);
-    });
-      result.forEach(user => {
       let cusEmail = customLowerCase(user.email);
       cusLowerCaseEmail.push(cusEmail)
-    });
-    console.log(toLowerCaseEmail);
-    console.log(cusLowerCaseEmail);
+    })
 
 
-}).catch(err => {
-  console.log(err)
-});
+    console.log('output',email)
+    console.log('output', cusLowerCaseEmail);
 
+  }catch (err){
+    console.log("error",err)
+  }
+
+})()
